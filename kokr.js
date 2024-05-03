4b2ca65cb7cad07e3f368f4f009bae39b2558e5f
ParkMini <parkteammini@gmail.com>
2024-05-03 10:33:49 +0900

    add: [콘솔] 출력, 에러출력 메서드 추가

---
+++ kokr.js
@@ -0,0 +1,17 @@
+class 콘솔 {
+  /**
+   * 웹 콘솔에 메시지를 출력합니다.
+   * @param {*} data
+   */
+  static 출력(data) {
+    console.log(data);
+  }
+
+  /**
+   * 웹 콘솔에 에러 메시지를 출력합니다.
+   * @param {*} data
+   */
+  static 에러출력(data) {
+    console.error(data);
+  }
+}
